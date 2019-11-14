88ad02950d00548b8f76cbc5c20710d95787d38f
Caleb López <caleb.lopez@ciacsacr.com>
2019-11-13 20:43:04 -0600

    Primer commit

---
+++ build/generated-sources/ap-source-output/com/ufidelitas/Articulo_.java
@@ -0,0 +1,15 @@
+package com.ufidelitas;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-13T19:46:04")
+@StaticMetamodel(Articulo.class)
+public class Articulo_ { 
+
+    public static volatile SingularAttribute<Articulo, String> descripcion;
+    public static volatile SingularAttribute<Articulo, Integer> codigo;
+    public static volatile SingularAttribute<Articulo, Double> precio;
+
+}
---
+++ src/java/com/ufidelitas/Articulo.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ufidelitas;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author reyner
+ */
+@Entity
+@Table(name = "ARTICULO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Articulo.findAll", query = "SELECT a FROM Articulo a")
+    , @NamedQuery(name = "Articulo.findByCodigo", query = "SELECT a FROM Articulo a WHERE a.codigo = :codigo")
+    , @NamedQuery(name = "Articulo.findByDescripcion", query = "SELECT a FROM Articulo a WHERE a.descripcion = :descripcion")
+    , @NamedQuery(name = "Articulo.findByPrecio", query = "SELECT a FROM Articulo a WHERE a.precio = :precio")})
+public class Articulo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "CODIGO")
+    private Integer codigo;
+    @Size(max = 30)
+    @Column(name = "DESCRIPCION")
+    private String descripcion;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "PRECIO")
+    private Double precio;
+
+    public Articulo() {
+    }
+
+    public Articulo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (codigo != null ? codigo.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Articulo)) {
+            return false;
+        }
+        Articulo other = (Articulo) object;
+        if ((this.codigo == null && other.codigo != null) || (this.codigo != null && !this.codigo.equals(other.codigo))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.ufidelitas.Articulo[ codigo=" + codigo + " ]";
+    }
+    
+}
